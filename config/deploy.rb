bc0f6370bfe2a9f369bd2750bbcc2fdbf7924993
russianblue <wlsdl0301@gmial.com>
2014-06-18 13:33:57 +0900

    test_depoly

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,6 +28,7 @@ set :repo_url, 'git@github.com:RussianBlue/t_process.git'
 
 # Default deploy_to directory is /var/www/my_app
 set :deploy_to, '/home/trigit/t_process'
+set :linked_files, %w{config/database.yml}
 
 # Default value for :scm is :git
 # set :scm, :git
