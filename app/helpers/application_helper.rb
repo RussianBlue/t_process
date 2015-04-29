2558f0f08faeb1038b00a8d25256cf052a1ff61e
russianblue <wlsdl0301@gmial.com>
2015-04-29 17:58:18 +0900

    aaaaaaaaaaaaaassibal

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,7 +20,9 @@ module ApplicationHelper
     end
   end
 
-  
+  def current_board_title(board_id)
+    boards_id = BoardCategory.find(board_id)
+  end
 
   def boolean_type_oxchange(arg)
     arg != true ? "X" : "O"
